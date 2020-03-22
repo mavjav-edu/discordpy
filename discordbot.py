81e4500d862a6f6b4717f696650ae2600e0526f2
Mavaddat Javid <mavaddat.javid@gmail.com>
2020-03-22 12:59:41 -0700

    quickstart example

--- discordbot.py
+++ discordbot.py
@@ -4,4 +4,14 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.)
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('bot-token')
