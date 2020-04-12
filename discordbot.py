afe3ac54c8a3ecf1b7ed0f836504f183bc8f53a9
Mavaddat Javid <mavaddat.javid@gmail.com>
2020-04-12 14:12:05 -0700

    simple bot in python

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,9 @@ import discord
 
 client = discord.Client()
 
+def play(parameter_list):
+    pass
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -11,7 +14,14 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
+    elif message.content.startswith('!play'):
+        if(message.content[5:].startswith('Music')):
+            await message.channel.send(message.content[10:]+' is not a good song, sorry!')
+        elif(message.content[5:].startswith('Movie')):
+            await message.channel.send(message.content[10:]+' is copyrighted, sorry!')
+        play(message.content)
 
-client.run('bot-token')
+token=open("token",'r')
+client.run(token.read())
