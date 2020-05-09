f088a47ca564358f204d7472be2d63fd283d38b9
Mavaddat Javid <mavaddat.javid@gmail.com>
2020-05-09 15:02:23 -0700

    added comments to explain complex parts

--- discordbot.py
+++ discordbot.py
@@ -33,9 +33,18 @@ async def on_message(message):
         await message.channel.send("I'm going...")
         await client.logout()
     elif message.content.lower().startswith('!remind'):
-        matches = re.match("\!remind\s*(\d+)\s*(.*)",message.content)
-        await message.channel.send("Sure. I'll remind you in "+matches[1]+" second"+('','s')[int(matches[1])>1])
-        asyncio.ensure_future(remind(int(matches[1]),message.channel,matches[2]))
+        pattern = re.compile("""
+        \$      # matches a literal '$' character
+        timer   # matches the word 'timer'
+        \s*     # matches zero or more whitespace characters
+        (\d+)   # capturing group 1: matches one or more digit characters
+        \s*     # matches zero or more whitespace characters
+        (.*)    # capturing group 2: matches zero or more of any character
+        """,re.VERBOSE)
+        matches = re.match(pattern,message.content) # `matches[0]` is the entire string matching the pattern; `matches[1]` is the first capture group; `matches[2]` is the second capture group
+        await message.channel.send("Sure. I'll remind you in "+matches[1]+" second"+('','s')[int(matches[1])>1]) # bot uses the correct plural or singular for time based on number of seconds specified
+        asyncio.ensure_future(remind(int(matches[1]),message.channel,matches[2])) # multithreaded function calls
+
     elif message.content.lower().startswith('!play'):
         if(message.content[5:].lower().startswith('music')):
             await message.channel.send(message.content[10:]+' is not a good song, sorry!')
--- writeToken.py
+++ writeToken.py
@@ -1,32 +1,32 @@
 import re
 from cryptography.fernet import Fernet
 
-try:
+try: # try-catch block for error `IOError`
     
-    key = open("key",'wb+')
-    token = open("token",'wb')
-    key.write(Fernet.generate_key())
-    key.seek(0)
-    f = Fernet(key.read())
+    key = open("key",'wb+') # attempts to create/open a 'key' file where we store the Fernet key (wb+ means Write Binary to file with additional read privileges)
+    token = open("token",'wb') # attempts to create/open a 'token' file where we store the encrypted Discord token (we don't need to read from 'token' here, so we only need write privileges)
+    key.write(Fernet.generate_key()) # generates a Fernet key and saves that key to the 'key' file
+    key.seek(0) # return the file pointer to the beginning (point 0) of the 'key' file (because the 'write(...)' command leaves the file pointer where it left off -- at the end of the file)
+    f = Fernet(key.read()) # creates a Fernet object to do the encryption using our key generated above
 
-    token.write(f.encrypt(str.encode(input("What is the discord bot token?> "))))
+    token.write(f.encrypt(str.encode(input("What is the discord bot token?> ")))) # asks the user for the Discord token, then encodes as binary, then encrypts the binary, and then writes binary to 'token' file
 
-    try:
-        gitignoref = open(".gitignore",'r')
-        gitignore = gitignoref.read()
-        gitignoref = open(".gitignore",'a')
-        keyRE = re.compile("key",re.MULTILINE)
-        tokenRE = re.compile("token",re.MULTILINE)
-        if(None == re.search(keyRE,gitignore)):
-            gitignoref.write("\nkey")
-        if(None == re.search(tokenRE,gitignore)):
-            gitignoref.write("\ntoken")
-    except IOError as error:
+    try: # reading and write `.gitignore` to make sure we don't accidentally upload key or token
+        gitignoref = open(".gitignore",'r') # opens `.gitignore` as a read only
+        gitignore = gitignoref.read() # stores the content of `.gitignore`
+        gitignoref = open(".gitignore",'a') # opens `.gitignore` append mode (write starting at the end of the file)
+        keyRE = re.compile("key",re.MULTILINE) # regular expression pattern matching the word 'key' anywhere
+        tokenRE = re.compile("token",re.MULTILINE) # regular expression pattern matching the word 'token' anywhere
+        if(None == re.search(keyRE,gitignore)): # if the word 'key' is not found in the content of `.gitignore`
+            gitignoref.write("\nkey") # then add 'key' to the next line in `.gitignore`
+        if(None == re.search(tokenRE,gitignore)): # if the word 'token' is not found in the content of `.gitignore`
+            gitignoref.write("\ntoken") # then add 'key' to the next line in `.gitignore`
+    except IOError as error: # catches `IOError` from trying to open `.gitignore`
         pass
-    finally:
-        gitignoref.close()
+    finally: # Below code will run in any event (whether there is an error or not)
+        gitignoref.close() # close the file `.gitignore`
 except IOError as error:
-    pass
-finally:
-    key.close()
-    token.close()
+    pass # This is the part where we recover from error `IOError`
+finally: # Below code will run in any event (whether there is an error or not)
+    key.close() # close the file `key`
+    token.close() # close the file `token`
