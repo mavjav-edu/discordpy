0f9feadc5c980fd1f35728172d027668a329b8b4
Mavaddat Javid <mavaddat.javid@gmail.com>
2020-05-16 18:00:21 -0700

    generalized writeToken

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-Mav Jav Education bot
-==========
+# Mav Jav Education bot
 
 <a href="https://discord.gg/KzzTBbr" alt="Discord server invite">![Discord server invite](https://discordapp.com/api/guilds/336642139381301249/embed.png)</a>
 <a href="https://pypi.python.org/pypi/discord.py"><object data="https://img.shields.io/pypi/v/discord.py.svg" type="image/svg+xml" alt="PyPI version info" height="20vh" align="initial"><img src="https://img.shields.io/pypi/v/discord.py.svg" alt="PyPI version info" height="20vh" align="initial"></object></a>
    
 [Mav Jav Education Discord server](https://discord.gg/KzzTBbr) bot. <img src="robot.png" alt="🤖" height="77vw" align="left">
 
-This project uses [Rapptz/discord.py](https://github.com/Rapptz/discord.py) to implement a [Discord](HTTPS://discord.gg) server bot. This bot is meant to be used as an educational tool to teach Python programming to grade 7-12 students<sup id="a1">[1](#f1)</sup>. deployed on [Mav Jav Education Discord server](https://discord.gg/KzzTBbr). 
+This project uses [Rapptz/discord.py](https://github.com/Rapptz/discord.py) to implement a [Discord](HTTPS://discord.gg) server bot. This bot is meant to be used as an educational tool to teach Python programming to grade 7-12 students<sup id="a1">[1](#f1)</sup>. The bot is currently deployed on [Mav Jav Education Discord server](https://discord.gg/KzzTBbr).
 
-## Build 
+## Build
 
-To assemble and deploy this bot for your own server, 
+To assemble and deploy this bot for your own server,
 
-1. Clone this repository and activate the virtual environment,
-2. Get a token from Discord
+1. Clone this repository
+2. Activate the virtual environment,
+3. Get a token from Discord
    1. Go to the [Discord developers' applications page](https://discord.com/developers/applications/)
    2. Create an application or click the application you created for this bot
    3. Click the 'Bot' tab with puzzle piece <object data="https://raw.githubusercontent.com/mavaddat-javid-education/discordpy/master/bot.svg" type="image/svg+xml" alt="'Bot' tab on Discord developer's application" height="20vh" align="initial"><img src="/bot.svg" alt="Bot' tab on Discord developer's application" height="20vh" align="initial"></object></a> icon 
@@ -21,6 +21,6 @@ To assemble and deploy this bot for your own server,
 
 ### run the [writeToken.py](writeToken.py) script to create an encrypted token and local key, then
 
-### run the [discordbot](discordbot.py) on your sever or computer.
+### run the [discordbot](discordbot.py) on your sever or computer
 
 <b id="f1">1</b> To use <a href="https://en.wikipedia.org/wiki/Discord">Discord™</a>, students must be <i>older</i> than the <u>age of digital consent</u>, which is 13 in Canada and USA.[↩](#a1)
--- writeToken.py
+++ writeToken.py
@@ -2,25 +2,30 @@ import re
 from cryptography.fernet import Fernet
 
 try: # try-catch block for error `IOError`
-    
-    key = open("key",'wb+') # attempts to create/open a 'key' file where we store the Fernet key (wb+ means Write Binary to file with additional read privileges)
-    token = open("token",'wb') # attempts to create/open a 'token' file where we store the encrypted Discord token (we don't need to read from 'token' here, so we only need write privileges)
-    key.write(Fernet.generate_key()) # generates a Fernet key and saves that key to the 'key' file
-    key.seek(0) # return the file pointer to the beginning (point 0) of the 'key' file (because the 'write(...)' command leaves the file pointer where it left off -- at the end of the file)
-    f = Fernet(key.read()) # creates a Fernet object to do the encryption using our key generated above
-
-    token.write(f.encrypt(str.encode(input("What is the discord bot token?> ")))) # asks the user for the Discord token, then encodes as binary, then encrypts the binary, and then writes binary to 'token' file
+    f=None
+    try: # This is for when the 'key' file already exists
+        key = open("key",'rb') # attempts to open a 'key' file where we store the Fernet key (rb means Read Binary from file)
+        f=Fernet(key.read()) # creates a Fernet object to do the encryption using our key generated above
+    except IOError as error: # This is for when the 'key' file doesn't exist or was deleted
+        key = open("key",'wb+') # attempts to create/open a 'key' file where we store the Fernet key (wb+ means Write Binary to file with additional read privileges)
+        key.write(Fernet.generate_key()) # generates a Fernet key and saves that key to the 'key' file
+        key.seek(0) # return the file pointer to the beginning (point 0) of the 'key' file (because the 'write(...)' command leaves the file pointer where it left off -- at the end of the file)
+        f = Fernet(key.read()) # creates a Fernet object to do the encryption using our key generated above 
+   
+    tokenFilename = input("What is the filename?> ")
+    token = open(tokenFilename,'wb') # attempts to create/open a 'tokenFilename' file where we store the encrypted Discord token (we don't need to read from 'tokenFilename' here, so we only need write privileges)
+    token.write(f.encrypt(str.encode(input("What is the secret?> ")))) # asks the user for the Discord token, then encodes as binary, then encrypts the binary, and then writes binary to 'token' file
 
     try: # reading and write `.gitignore` to make sure we don't accidentally upload key or token
         gitignoref = open(".gitignore",'r') # opens `.gitignore` as a read only
         gitignore = gitignoref.read() # stores the content of `.gitignore`
         gitignoref = open(".gitignore",'a') # opens `.gitignore` append mode (write starting at the end of the file)
         keyRE = re.compile("key",re.MULTILINE) # regular expression pattern matching the word 'key' anywhere
-        tokenRE = re.compile("token",re.MULTILINE) # regular expression pattern matching the word 'token' anywhere
+        tokenRE = re.compile(tokenFilename,re.MULTILINE) # regular expression pattern matching the word 'token' anywhere
         if(None == re.search(keyRE,gitignore)): # if the word 'key' is not found in the content of `.gitignore`
             gitignoref.write("\nkey") # then add 'key' to the next line in `.gitignore`
         if(None == re.search(tokenRE,gitignore)): # if the word 'token' is not found in the content of `.gitignore`
-            gitignoref.write("\ntoken") # then add 'key' to the next line in `.gitignore`
+            gitignoref.write("\n"+tokenFilename) # then add 'key' to the next line in `.gitignore`
     except IOError as error: # catches `IOError` from trying to open `.gitignore`
         pass
     finally: # Below code will run in any event (whether there is an error or not)
@@ -30,3 +35,11 @@ except IOError as error:
 finally: # Below code will run in any event (whether there is an error or not)
     key.close() # close the file `key`
     token.close() # close the file `token`
+
+#after using this script, the `token` file will be useable like this:
+"""
+key=open("key",'rb')
+fernet=Fernet(key.read())
+token=open("token",'rb')
+client.run((fernet.decrypt(token.read())).decode())
+"""
